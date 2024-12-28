f321c2c11b934be472d1bc54cc88109ffe3a9441
Daniel Oliveira <danielmoliveira@outlook.com>
2024-12-28 06:49:41 -0500

    Changes on gui_commands and added api_commands

--- cypress/e2e/gui/login.cy.js
+++ cypress/e2e/gui/login.cy.js
@@ -1,6 +1,10 @@
-describe('login', () => {
-  it('sucessffully', () => {
-    cy.login()
+describe('Login', () => {
+  it('successfully', () => {
+    const user = Cypress.env('user_name')
+    const password = Cypress.env('user_password')
+    const options = { cacheSession: false }
+
+    cy.login(user, password, options)
 
     cy.get('.qa-user-avatar').should('be.visible')
   })
