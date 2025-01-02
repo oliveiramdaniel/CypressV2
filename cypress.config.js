f881ecbaa2bbc00ea6df91b996403bd88462b9a6
Daniel Oliveira <danielmoliveira@outlook.com>
2025-01-02 07:37:58 -0500

    final version for this project

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,11 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080',
+    env: {
+      hideCredentials: true,
+      requestMode: true,
+    },
+    experimentalRunAllSpecs: true
   },
   fixturesFolder: false,
   video: false,
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,5 @@
+import 'cypress-plugin-api'
+
+import './cli_commands'
 import './api_commands'
 import './gui_commands'
